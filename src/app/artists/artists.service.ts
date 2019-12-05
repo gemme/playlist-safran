7e23a236465e6b034c12d4d9f400a25af0d386b3
Joel Artuo Alvarez Hernandez <joelaahdz@hotmail.com>
2019-12-04 18:14:30 -0700

    avance servicio => tercera parte

--- src/app/artists/artists.service.ts
+++ src/app/artists/artists.service.ts
@@ -11,9 +11,9 @@ export class ArtistsService {
 
 
   SearchArtist(artists:string){
-    const api_key = '291279da08301e10d6434267511d72f2';
-    const api_url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artists}&api_key=${api_key}&format=json`;
-    let response = this.httpclient.get(api_url).toPromise();
-    console.log(response);
+    const API_KEY = '291279da08301e10d6434267511d72f2';
+    const API_URL = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artists}&api_key=${API_KEY}&format=json`;
+    const RESPONSE = this.httpclient.get(API_URL).toPromise();
+    console.log(RESPONSE);
   }
 }
